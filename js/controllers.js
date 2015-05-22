c29c2f2e828ad5603547ea826c246d87d9d5ae0c
araju <akhilraju@gmail.com>
2015-05-22 00:02:20 -0400

    changed style of site, and some content on index

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,44 @@
-var imagesApp = angular.module('imagesApp', []);
+var imagesApp = angular.module('imagesApp', ['infinite-scroll']);
 
 imagesApp.controller('ImageListCtrl', ['$scope', '$http',
+  
   function ($scope, $http) {
+    angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 500);
+    $scope.images = [];
+    $scope.allImages = [];
     $http.get('data/images.json').success(function(data) {
-      $scope.images = data;
+      $scope.allImages = data;
+      //$scope.images = $scope.allImages.slice(0,12);
+      for (var i = 0; i < 5; i++) {
+        $scope.images.push($scope.allImages[i]);
+      }
     });
     $http.get('data/datasets.json').success(function(data) {
       $scope.datasets = data;
       $scope.selectedDataset = $scope.datasets[0];
     });
-
+    // $scope.images = [];
     $scope.sortStat = 'memscore';
     $scope.reverse = true;
-  }]);
+
+    
+
+    $scope.loadMore = function() {
+      console.log("loading more");
+      var last = $scope.images.length;
+      if (last < $scope.allImages.length) {
+        for(var i = 0; i < 5; i++) {
+          $scope.images.push($scope.allImages[last + i]);
+        }
+      }
+    };
+    
+
+
+    // $scope.loadMore = function() {
+    //   var last = $scope.images[$scope.images.length - 1];
+    //   for(var i = 1; i <= 8; i++) {
+    //     $scope.images.push(last + i);
+    //   }
+    // };
+}]);
